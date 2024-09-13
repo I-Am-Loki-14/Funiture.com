f02e8ac6574dc3403f85ec6344b4c60d0e6a123d
Min Naing Oo <minnaingoo792@gmail.com>
2024-09-13 15:14:17 +0630

    Funiture.com_done

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,4 @@
+// Menu bar 
 const navmenuimgbar =document.getElementById('nav-menu-img-bar');
 console.log(navmenuimgbar);
 const navlists =document.getElementById('nav-list');
@@ -12,3 +13,39 @@ navmenuimgbar.addEventListener('click',()=>{
           navmenuimgbar.src='./img/Menu.svg';
    }
 })
+// show more 
+const mainrightdescriptiondeliveryimg = document.getElementById('main-right-description-delivery-img');
+const mainrightdescriptiondeliveryspan=document.getElementById('main-right-description-delivery-span');
+
+mainrightdescriptiondeliveryimg.addEventListener('click',()=>{
+       mainrightdescriptiondeliveryspan.classList.toggle('showmore');
+
+       if (mainrightdescriptiondeliveryspan.classList.contains('showmore')) {
+              mainrightdescriptiondeliveryimg.src="img/ArrowBackward.svg";
+       } else {
+              mainrightdescriptiondeliveryimg.src="img/ArrowForward.svg";
+       }
+
+})
+
+
+
+
+const mainleftuprbutton = document.querySelectorAll('.main-left-up-r button');
+console.log(mainleftuprbutton);
+
+mainleftuprbutton.forEach(button => {
+
+       button.addEventListener('click', () => {
+              console.log('button clicked');
+              mainleftuprbutton.forEach(button => {
+                     button.style.border = 'none';
+              
+              });
+              button.style.border = '2px solid black';
+              
+       });
+});
+
+
+
